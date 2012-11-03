23ef07e1f6c55b74054d3df291f3baf3c68ee361
MorganBorman <morgan.borman@gmail.com>
2012-11-03 11:58:45 -0700

    Added two project templates which help test the project running system.

---
+++ data/templates/looping_hellos/looping_hellos.cpp
@@ -0,0 +1,12 @@
+// my first program in C++
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    for(int i = 0; i <= 1000; i++) {
+      cout << "Hello World: " << i << "\n";
+    }
+  return 0;
+}
