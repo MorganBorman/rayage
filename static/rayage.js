a9bae2add1484fac5ab3c044dea2d97444ac9a5a
MorganBorman <morgan.borman@gmail.com>
2012-12-10 20:04:31 -0800

    Added functionality groups which can be toggled on and off in the ProjectMenu widget. Added a loading spinner.

--- static/rayage.js
+++ static/rayage.js
@@ -1,4 +1,13 @@
-require(["dojo/parser", "dojo/domReady!", "custom/Rayage"],
-function(parser){
+require(["dojo/parser", "dojo/_base/fx", "dojo/dom-style", "dojo/domReady!", "custom/Rayage"],
+function(parser, fx, style){
     parser.parse();
+    
+    var fadeArgs = {
+        node: "preloader",
+        duration: 1000
+    };
+    fx.fadeOut(fadeArgs).play();
+    setTimeout(function() {
+        style.set("preloader", "display", "none");
+    }, 1000)
 });
