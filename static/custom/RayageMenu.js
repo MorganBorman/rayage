df749f9b0d75a6569957a0018b1b5c04a171bd74
MorganBorman <morgan.borman@gmail.com>
2012-12-10 18:04:59 -0800

    topic publishing is now automatically hooked up to the menu items.

--- static/custom/RayageMenu.js
+++ static/custom/RayageMenu.js
@@ -1,7 +1,9 @@
 // custom.RayageMenu
 define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin", "dojo/text!./templates/RayageMenu.html",
+        "dojo/on", "dojo/topic", "dijit/registry", "dojo/dom-attr", 
         "dijit/MenuBar", "dijit/PopupMenuBarItem", "dijit/DropDownMenu", "dijit/MenuItem", "dijit/MenuSeparator", "dijit/MenuBarItem"],
-    function(declare, WidgetBase, TemplatedMixin, WidgetsInTemplateMixin, template) {
+    function(declare, WidgetBase, TemplatedMixin, WidgetsInTemplateMixin, template,
+             on, topic, registry, domAttr) {
         return declare([WidgetBase, TemplatedMixin, WidgetsInTemplateMixin], {
             // Our template - important!
             templateString: template,
@@ -20,6 +22,35 @@ define(["dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dij
             
             startup: function() {
                 this.menu_bar.startup();
+                
+                var topic_hookup = function(widget) {
+                    on(widget, "click", function() {
+                        topic.publish(widget.topic);
+                    });
+                }
+                
+                // Recursively searches for all widgets which have a 'topic' attribute
+                // and hooks up them to public on clicking.
+                var recursive_topic_hookup;
+                recursive_topic_hookup = function(widget) {
+                    if (typeof widget.topic != "undefined") {
+                        topic_hookup(widget);
+                    }
+                    
+                    if (typeof widget.popup != "undefined") {
+                        var popupchildren = widget.popup.getChildren();
+                        for(var i = 0; i < popupchildren.length; i++) {
+                            recursive_topic_hookup(popupchildren[i]);
+                        }
+                    }
+                
+                    var subwidgets = registry.findWidgets(widget.domNode);
+                    for(var i = 0; i < subwidgets.length; i++) {
+                        recursive_topic_hookup(subwidgets[i]);
+                    }
+                }
+                
+                recursive_topic_hookup(this.menu_bar);
             },
             
             // The constructor
